a682dd3ec27e8ac5acd5d74eef34dd7a72a2a920
hoppibot <70176791+hoppibot@users.noreply.github.com>
2020-08-27 14:11:37 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,3 +2,13 @@ function scrollToFeatures() {
     features = document.getElementById('featuresHeader')
     features.scrollIntoView()
 }
+
+function scrollToFeedback() {
+    features = document.getElementById('feedbackHeader')
+    features.scrollIntoView()
+}
+
+function scrollToVerify() {
+    features = document.getElementById('verifyHeader')
+    features.scrollIntoView()
+}
