fc8cc14f11d82a8078737b215d5d46563bb75804
hoppibot <70176791+hoppibot@users.noreply.github.com>
2020-09-13 08:45:11 -0700

    Added light mode function

--- scripts.js
+++ scripts.js
@@ -17,3 +17,63 @@ function scrollToLinks() {
     features = document.getElementById('linksHeader')
     features.scrollIntoView()
 }
+
+function darkmodeOnClick() {
+    let e = document.body;
+    let e2 = document.getElementById('light')
+    let selects = document.getElementsByClassName('featureStyleTitle');
+    let more = document.getElementsByTagName('h1');
+    let desc = document.getElementsByClassName('featureStyleDesc')
+    let main = document.getElementById('navigationMain')
+    let para = document.getElementsByTagName('p')
+
+    if (e2.innerHTML === 'Light Mode') {
+
+        let navigation = document.getElementsByClassName('navigation')
+
+        e2.innerHTML = 'Light Mode On'
+        e.style.background = 'white'
+        main.style.color = 'black'
+        for (i = 0; i < more.length; i++) {
+            more[i].style.color = 'black'
+        }
+        for (i = 0; i < desc.length; i++) {
+            desc[i].style.color = 'black'
+        }
+        for (i = 0; i < selects.length; i++) {
+            selects[i].style.color = 'black'
+        }
+        for (i = navigation.length - 1; i >= 0; i--) {
+            // console.info(navigation)
+            navigation[i].className = 'navigationDark'
+        }
+        for (i = 0; i < para.length; i++) {
+            para[i].style.color = 'black'
+        }
+    
+    } else if (e2.innerHTML === 'Light Mode On') {
+
+        e2.innerHTML = 'Light Mode'
+        e.style.background = '#131B21'
+            main.style.color = 'white'
+        
+        let navigation = document.getElementsByClassName('navigationDark')
+        
+        for (i = 0; i < more.length; i++) {
+            more[i].style.color = 'white'
+        }
+        for (i = 0; i < desc.length; i++) {
+            desc[i].style.color = '#aeaeb0'
+        }
+        for (i = 0; i < selects.length; i++) {
+            selects[i].style.color = 'white'
+        }
+        for (i = navigation.length - 1; i >= 0; i--) {
+            navigation[i].className = 'navigation'
+        }
+        for (i = 0; i < para.length; i++) {
+            para[i].style.color = '#BDBDBF'
+        }
+    
+    }
+}
